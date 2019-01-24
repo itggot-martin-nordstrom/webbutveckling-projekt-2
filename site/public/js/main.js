7fb94951843c1c6b50b673ae036a7225504fa640
itggot-martin-nordstrom <martin.nordstrom2@elev.ga.ntig.se>
2019-01-24 15:40:35 +0100

    created get-here.html. Added JS element to and between sites

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -5,3 +5,20 @@ function SidebarToggle(event){
     let sidebarfade = document.querySelector(".sidebar-fade")
     sidebarfade.classList.toggle("SidebarFadeToggle")
 }
+
+function SaveItems(){
+    var start = document.querySelector("#startLocation");
+    window.localStorage.setItem("start", start.value);
+    var end = document.querySelector("#endLocation");
+    window.localStorage.setItem("end", end.value);
+
+    document.querySelector("#get-here-start").innerHTML = window.localStorage.getItem("start");
+    document.querySelector("#get-here-end").innerHTML = window.localStorage.getItem("end");
+}
+
+console.log("SCRIPT LOADED")
+
+
+document.querySelector("#get-here-start").innerHTML = window.localStorage.getItem("start");
+document.querySelector("#get-here-end").innerHTML = window.localStorage.getItem("end");
+
