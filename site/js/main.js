e4741515f508d1004393828cdc6288aa9acd65f8
itggot-martin-nordstrom <martin.nordstrom2@elev.ga.ntig.se>
2019-01-26 17:21:02 +0100

    fixed all media widths and made sure everything worked as intended. finished get-here.html with elements and JavaScript

--- site/js/main.js
+++ site/js/main.js
@@ -9,16 +9,42 @@ function SidebarToggle(event){
 function SaveItems(){
     var start = document.querySelector("#startLocation");
     window.localStorage.setItem("start", start.value);
+    
     var end = document.querySelector("#endLocation");
     window.localStorage.setItem("end", end.value);
 
+    
     document.querySelector("#get-here-start").innerHTML = window.localStorage.getItem("start");
     document.querySelector("#get-here-end").innerHTML = window.localStorage.getItem("end");
+    document.querySelector("#get-here-start1").innerHTML = window.localStorage.getItem("start");
+    document.querySelector("#get-here-end1").innerHTML = window.localStorage.getItem("end");
+    document.querySelector("#get-here-start2").innerHTML = window.localStorage.getItem("start");
+    document.querySelector("#get-here-end2").innerHTML = window.localStorage.getItem("end");
+    document.querySelector("#get-here-start3").innerHTML = window.localStorage.getItem("start");
+    document.querySelector("#get-here-end3").innerHTML = window.localStorage.getItem("end");
+    document.querySelector("#get-here-start4").innerHTML = window.localStorage.getItem("start");
+    document.querySelector("#get-here-end4").innerHTML = window.localStorage.getItem("end");
+    document.querySelector("#get-here-start5").innerHTML = window.localStorage.getItem("start");
+    document.querySelector("#get-here-end5").innerHTML = window.localStorage.getItem("end");
+    document.querySelector("#get-here-start6").innerHTML = window.localStorage.getItem("start");
+    document.querySelector("#get-here-end6").innerHTML = window.localStorage.getItem("end");
 }
 
 console.log("SCRIPT LOADED")
 
-
 document.querySelector("#get-here-start").innerHTML = window.localStorage.getItem("start");
 document.querySelector("#get-here-end").innerHTML = window.localStorage.getItem("end");
+document.querySelector("#get-here-start1").innerHTML = window.localStorage.getItem("start");
+document.querySelector("#get-here-end1").innerHTML = window.localStorage.getItem("end");
+document.querySelector("#get-here-start2").innerHTML = window.localStorage.getItem("start");
+document.querySelector("#get-here-end2").innerHTML = window.localStorage.getItem("end");
+document.querySelector("#get-here-start3").innerHTML = window.localStorage.getItem("start");
+document.querySelector("#get-here-end3").innerHTML = window.localStorage.getItem("end");
+document.querySelector("#get-here-start4").innerHTML = window.localStorage.getItem("start");
+document.querySelector("#get-here-end4").innerHTML = window.localStorage.getItem("end");
+document.querySelector("#get-here-end4").innerHTML = window.localStorage.getItem("end");
+document.querySelector("#get-here-start5").innerHTML = window.localStorage.getItem("start");
+document.querySelector("#get-here-end5").innerHTML = window.localStorage.getItem("end");
+document.querySelector("#get-here-start6").innerHTML = window.localStorage.getItem("start");
+document.querySelector("#get-here-end6").innerHTML = window.localStorage.getItem("end");
 
